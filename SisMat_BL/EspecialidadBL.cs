51cda3d67edcd8719068436a1b51da66cb05358d
NakataModem <bruno.raff1@hotmail.com>
2022-10-20 13:43:41 -0500

    Clases de BE y BL

---
+++ SisMat_BL/EspecialidadBL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Agregar...
+using SisMat_ADO;
+namespace SisMat_BL
+{
+    public  class EspecialidadBL
+    {
+        EspecialidadADO objEspecialidadADO = new EspecialidadADO();
+
+        public DataTable ListarEspecialidad()
+        {
+            return objEspecialidadADO.ListarEspecialidad();
+        }
+    }
+}
