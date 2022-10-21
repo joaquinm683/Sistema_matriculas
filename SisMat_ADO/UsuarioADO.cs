e12ef91344986d9d45b5ce14f97d0719a8f1b6d1
stevenht19 <mstvdev19@gmail.com>
2022-10-21 16:58:10 -0500

    Agregar metodos en las clases ADO

---
+++ SisMat_ADO/UsuarioADO.cs
@@ -0,0 +1,55 @@
+﻿using SisMat_BE;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SisMat_ADO
+{
+    public class UsuarioADO
+    {
+        ConexionADO ADOconnection = new ConexionADO();
+        SqlConnection sqlConnection = new SqlConnection();
+        SqlCommand command = new SqlCommand();
+        SqlDataReader dr;
+
+        public UsuarioBE ConsultarUsuario(String strCodigo)
+        {
+            try
+            {
+                UsuarioBE objUsuarioBE = new UsuarioBE();
+
+                sqlConnection.ConnectionString = ADOconnection.GetCnx();
+                command.Connection = sqlConnection;
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "usp_ConsultarUsuario";
+                command.Parameters.Clear();
+                // command.Parameters.AddWithValue("param", strCodigo);
+
+                sqlConnection.Open();
+                dr = command.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    dr.Read();
+                    objUsuarioBE.Username = dr["Username"].ToString();
+                    objUsuarioBE.password = dr["password"].ToString();
+                    objUsuarioBE.Est_usuario = Convert.ToInt16(dr["Est_usuario"]);
+
+                }
+                return objUsuarioBE;
+            } catch (SqlException e)
+            {
+                throw new Exception(e.Message);
+            } finally
+            {
+                if (sqlConnection.State == ConnectionState.Open)
+                {
+                    sqlConnection.Close();
+                }
+            }
+        }
+    }
+}
